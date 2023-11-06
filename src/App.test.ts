0d188cfb93bc1af875e919546ef56f81c8667d0e
Jeandre Rohlandt <jrohlandt@gmail.com>
2023-11-06 10:36:53 +0200

    change test to break it

--- src/App.test.ts
+++ src/App.test.ts
@@ -2,6 +2,6 @@ import { describe, expect, it } from 'vitest';
 
 describe('Test app', () => {
   it('should be able to add two numbers', () => {
-    expect(1 + 1).toBe(2);
+    expect(1 + 1).toBe(1);
   });
 });
